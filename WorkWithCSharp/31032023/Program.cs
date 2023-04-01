a86f6bf810b1d0c2e4d406b609b04e13c487d7ac
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-04-01 21:30:03 +0200

    методы

--- WorkWithCSharp/31032023/Program.cs
+++ WorkWithCSharp/31032023/Program.cs
@@ -10,6 +10,43 @@ namespace _31032023
     {
         static void Main(string[] args)
         {
+            int[] firstArray = new int[10];
+            int[] secondArray = new int[20];
+
+            firstArray = FillArray(firstArray.Length);
+            secondArray = FillArray(secondArray.Length);
+
+            Console.WriteLine(Join("#", firstArray));
+            Console.WriteLine(Join("?", secondArray));
+
+
+
+        int[] FillArray(int arrayLength)
+        {
+            int[] array = new int[arrayLength];
+            for (int i = 0; i < arrayLength; i++)
+            {
+                array[i] = new Random().Next(0, 100);
+            }
+            return array;
+        }
+
+            
+        string Join(string separator, int[] array)
+        {
+            string result = "";
+
+            foreach(int item in array)
+            {
+                result += item + separator;
+            }
+            return result;
+        }
+
+
+
+
+
         }
     }
 }
