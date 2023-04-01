636a624ecbed6c395c803d979693ed38a09b6e5b
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-04-01 16:34:04 +0200

    Файлы

---
+++ WorkWithCSharp/FileWork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _directory = Directory.GetCurrentDirectory();
+            Console.WriteLine(_directory);
+            string _fileAddressFirstWay = Directory.GetParent(_directory).Parent.Parent.FullName;
+            Console.WriteLine(_fileAddressFirstWay);
+            string _fileAddressSecondWay = new DirectoryInfo(_directory).Parent.Parent.Parent.FullName;
+            Console.WriteLine(_fileAddressSecondWay);
+        }
+    }
+}
