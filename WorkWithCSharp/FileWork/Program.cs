e9211410c598630fdc4c09f99fa889b9394e9f29
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-04-01 17:53:24 +0200

    Добавлены новые методы по дз

--- WorkWithCSharp/FileWork/Program.cs
+++ WorkWithCSharp/FileWork/Program.cs
@@ -17,7 +17,7 @@ namespace FileWork
             List<string> _data = new List<string>();
             bool _isExit = false;
 
-            string[] _commandList = new string[] { "exit", "remove", "help", "?", "uppercase" };
+            string[] _commandList = new string[] { "exit", "remove", "help", "?", "uppercase", "alllines", "sumlines", "sumnumbers" };
 
 
             _data = ReadFile();
@@ -62,6 +62,15 @@ namespace FileWork
                     case "uppercase":
                         UpperCaseLine();
                         break;
+                    case "alllines":
+                        AllLines();
+                        break;
+                    case "sumlines":
+                        ConcatTwoLines();
+                        break;
+                    case "sumnumbers":
+                        SumTwoNumbers();
+                        break;
                 }
 
             }
@@ -103,10 +112,70 @@ namespace FileWork
                 Console.WriteLine("Файл сохранен");
             }
 
+            // 1. Метод, выводящий общее количество строк в файле
+            void AllLines()
+            {
+                Console.WriteLine($"В файле содержится {_data.Count} строк") ;
+            }
+
+
+            // 2. Метод, складывающий две строки
+            void ConcatTwoLines()
+            {
+                Console.WriteLine("Введите номера двух строк");
+                int firstNumber = int.Parse(Console.ReadLine());
+                int secondNumber = int.Parse(Console.ReadLine());
+
+                if (firstNumber < _data.Count && secondNumber < _data.Count)
+                {
+                    Console.WriteLine($"В результате конкантенации получено вот что: {_data[firstNumber] + _data[secondNumber]}");
+                }
+                else
+                {
+                    Console.WriteLine("Строка(и) не найдена(ы), возврат в основное меню");
+                }
+            }
+
+
+
+            // 3. Метод, вычисляющий сумму двух чисел, содержащихся в файле
+            void SumTwoNumbers()
+            {
+                int firstNumber, secondNumber;
+               
+
+                Console.WriteLine("Введите номера искомых строк");
+                int firstLine = int.Parse(Console.ReadLine());
+                int secondLine = int.Parse(Console.ReadLine());
+
+                if (firstLine < _data.Count && secondLine < _data.Count)
+                {
+                    if (int.TryParse(_data[firstLine], out firstNumber) && int.TryParse(_data[secondLine], out secondNumber))
+                    {
+                        Console.WriteLine($"Сумма двух чисел равна {firstNumber + secondNumber}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не все выбранные строки - числа, возврат в основное меню");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Строка(и) не найдена(ы), возврат в основное меню");
+                }
+            }
+
+
+
             void HelperMethod()
             {
-                Console.WriteLine("Вам доступны следующие команды -\n1.remove - удаление строки (требует ввод номера строки)" +
-                    "\n2.exit - завершает выполнение программы и сохраняет изменения в файле");
+                Console.WriteLine("Вам доступны следующие команды:" +
+                    "\n1.remove - удаление строки (требует ввод номера строки)" +
+                    "\n2.exit - завершает выполнение программы и сохраняет изменения в файле" +
+                    "\n3.uppercase - перезаписывает содержимое текстовой строки в верхнем регистре" +
+                    "\n4.alllines - выводит количество строчек в файле" +
+                    "\n5.sumlines - осуществляет конкантенацию двух выбранных строчек" +
+                    "\n6.sumnumbers - осуществляет операцию сложения двух чисел из содержимого файла");
             }
 
             void RemoveLines()
