3fda0ec93122794592501e792c9dbf50cc10f737
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-12 17:06:22 +0200

    Update Program.cs

--- WorkWithCSharp/SortingAlgorithms/Program.cs
+++ WorkWithCSharp/SortingAlgorithms/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace SortingAlgorithms
 {
+    // Текущий проект содержит в себе основные методы сортировки массивов
+    // Current project contains all main sorting algorithms for numeric arrays
     class Program
     {
         static void Main(string[] args)
         {
+        }
+
+        // Алгоритм сортировки пузырьком
+        int[] BubbleSort(int[] array)
+        {
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                for(int j = i + 1; j < array.Length; j++)
+                {
+                    if(array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+
+
+        // Вспомогательная функция по созданию случайного массива
+        // Adding function using for fast creation of user array
+        int[] CreateArray(int arraySize, int lowestElementValue, int highestElementValue)
+        {
+            int[] array = new int[arraySize];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(lowestElementValue, highestElementValue);
+            }
+            return array;
         }
     }
 }
