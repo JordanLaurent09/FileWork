a7422dbc46d90479b9cc6e6fde7eeddc4c3544b3
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-13 16:38:07 +0200

    сортировка выбором

--- WorkWithCSharp/SortingAlgorithms/Program.cs
+++ WorkWithCSharp/SortingAlgorithms/Program.cs
@@ -14,7 +14,8 @@ namespace SortingAlgorithms
         {
             int[] array = CreateArray(20, 0, 21);
             PrintArray(array);
-            array = BubbleSort(array);
+            //array = BubbleSort(array);
+            array = SelectionSort(array);
             Console.WriteLine();
             PrintArray(array);
         }
@@ -35,6 +36,32 @@ namespace SortingAlgorithms
                     }
                 }
             }
+            return array;
+        }
+
+
+        // Алгоритм сортировки выбором
+
+        static int[] SelectionSort(int[] array)
+        {
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                int nMin = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if(array[nMin] > array[j])
+                    {
+                        nMin = j;
+                    }
+                }
+                if (nMin != i)
+                {
+                    int temp = array[nMin];
+                    array[nMin] = array[i];
+                    array[i] = temp;
+                }
+            }
+
             return array;
         }
 
