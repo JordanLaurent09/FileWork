2eccb4bef2a79a21159fb7ed110c105a15080dba
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-16 16:31:43 +0200

    Update Program.cs

--- WorkWithCSharp/SomeArrayFunctions/Program.cs
+++ WorkWithCSharp/SomeArrayFunctions/Program.cs
@@ -10,6 +10,29 @@ namespace SomeArrayFunctions
     {
         static void Main(string[] args)
         {
+            
+        }
+
+
+        static void PrintArray(int[] array)
+        {
+            foreach(int item in array)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+
+        static int[] CreateArray(int arrayLength, int lowerLimit, int upperLimit)
+        {
+            Random random = new Random();
+            int[] array = new int[arrayLength];
+            for(int i = 0; i < arrayLength; i++)
+            {
+                array[i] = random.Next(lowerLimit, upperLimit);
+            }
+
+            return array;
         }
     }
 }
