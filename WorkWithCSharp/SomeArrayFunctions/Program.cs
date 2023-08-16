44cb492c09cffef038d1085fef222a3ada50ac79
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-16 17:36:48 +0200

    Некоторые методы Array

--- WorkWithCSharp/SomeArrayFunctions/Program.cs
+++ WorkWithCSharp/SomeArrayFunctions/Program.cs
@@ -11,33 +11,41 @@ namespace SomeArrayFunctions
         static void Main(string[] args)
         {
             int[] array = CreateArray(20, 0, 28);
+            Console.WriteLine("Исходный массив:");
             PrintArray(array);
 
             //1. Метод Clear позволяет удалять элементы, входящие в выбранный диапазон номеров индексов массива
             Array.Clear(array, 0, array.Length);
+            Console.WriteLine("Результат работы метода Clear:");
             PrintArray(array);
 
             array = CreateArray(20, 0, 28);
+            Console.WriteLine("Исходный массив:");
             PrintArray(array);
 
             //2. Метод Reverse переставляет все элементы массива в обратном порядке
             Array.Reverse(array);
+            Console.WriteLine("Результат работы метода Reverse:");
             PrintArray(array);
 
             array = CreateArray(20, 0, 28);
+            Console.WriteLine("Исходный массив:");
             PrintArray(array);
 
             //3. Метод Sort помогает отсортировать элементы массива
             Array.Sort(array);
+            Console.WriteLine("Результат работы метода Sort:");
             PrintArray(array);
 
 
             //4. Метод BinarySearch ищет и возвращает индекс искомого числа в предварительно отсортированном массиве;
             // в случае отсутствия данного числа метод возвращает отрицательное значение
+            Console.WriteLine("Результат работы метода BinarySearch:");
             Console.WriteLine(Array.BinarySearch(array, 22));
 
 
             array = CreateArray(20, 0, 28);
+            Console.WriteLine("Исходный массив:");
             PrintArray(array);
 
 
@@ -48,27 +56,30 @@ namespace SomeArrayFunctions
 
             // Теперь применим метод ConstrainedCopy
             Array.ConstrainedCopy(array, 3, arrayTwo, 3, 4);
+            Console.WriteLine("Результат работы метода ConstraintCopy:");
             PrintArray(arrayTwo);
 
 
             array = CreateArray(20, 0, 28);
+            Console.WriteLine("Исходный массив:");
             PrintArray(array);
 
             //6. Метод IndexOf ищет заданное значение внутри массива и возвращает индекс его первого вхождения в массив
-
+            Console.WriteLine("Результат работы метода IndexOf:");
             Console.WriteLine(Array.IndexOf(array, 21));
 
 
             //7. Метод LastIndexOf возвращает индекс последнего вхождения искомого числа в массив
-
+            Console.WriteLine("Результат работы метода LastIndexOf:");
             Console.WriteLine(Array.LastIndexOf(array, 20));
 
             //8. Метод Resize изменяет размер массива до указанной величины
             Array.Resize(ref array, 13);
-
+            Console.WriteLine("Новый размер массива:");
             Console.WriteLine(array.Length);
-
+            Console.WriteLine("Результат работы метода Resize:");
             PrintArray(array);
+            
         }
 
 
@@ -76,7 +87,7 @@ namespace SomeArrayFunctions
         // Вспомогательная функция для вывода массива на экран
         static void PrintArray(int[] array)
         {
-            foreach(int item in array)
+            foreach (int item in array)
             {
                 Console.Write($"{item} ");
             }
