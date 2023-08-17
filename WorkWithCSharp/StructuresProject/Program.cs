1c4cb7f17be18626834190334ef443ed34d24a31
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-17 17:11:50 +0200

    Работа со структурами

--- WorkWithCSharp/StructuresProject/Program.cs
+++ WorkWithCSharp/StructuresProject/Program.cs
@@ -8,8 +8,22 @@ namespace StructuresProject
 {
     class Program
     {
+        struct People
+        {
+            public string name;
+            public string surname;
+        }
         static void Main(string[] args)
         {
+            // Первый способ инициализации структуры
+            People personOne = new People();
+            personOne.name = "Andrew";
+            personOne.surname = "Klein";
+            Console.WriteLine($"{personOne.name} {personOne.surname}");
+
+            // Второй способ инициализации структуры
+            People personTwo = new People() { name = "Richard", surname = "Robinson" };
+            Console.WriteLine($"{personTwo.name} {personTwo.surname}");
         }
     }
 }
