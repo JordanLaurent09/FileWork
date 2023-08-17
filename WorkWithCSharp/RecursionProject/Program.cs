51be74218d82e726048e390b7b1096c26e5aa4cd
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-17 16:37:32 +0200

    немного рекурсии

--- WorkWithCSharp/RecursionProject/Program.cs
+++ WorkWithCSharp/RecursionProject/Program.cs
@@ -10,7 +10,10 @@ namespace RecursionProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Вычисление факториала:");
             Console.WriteLine(RecFactorial(5));
+            Console.WriteLine("Возведение числа в степень:");
+            Console.WriteLine(Power(5, 2));
         }
 
         // Рекурсивная функция по вычислению факториала
@@ -18,6 +21,15 @@ namespace RecursionProject
         {
             if (n == 0 || n == 1) return 1;
             return n * RecFactorial(n - 1);
+        }
+
+        // Рекурсивная функция по вычислению степени числа
+
+        static long Power(int number, int power)
+        {
+            if (power == 0) return 1;
+            if (power == 1) return number;
+            return number * Power(number, power - 1);
         }
     }
 }
