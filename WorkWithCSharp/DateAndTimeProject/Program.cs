a62b80920ef129e69ceb0a23870749773384df74
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-18 15:46:27 +0200

    Update Program.cs

--- WorkWithCSharp/DateAndTimeProject/Program.cs
+++ WorkWithCSharp/DateAndTimeProject/Program.cs
@@ -11,7 +11,7 @@ namespace DateAndTimeProject
         static void Main(string[] args)
         {
 
-            // Создание объекта типа "дата-время"
+            // Создание объекта по умолчанию типа "дата-время"
             DateTime dateTime = new DateTime();
             Console.WriteLine($"Значение по умолчанию: {dateTime}");
 
@@ -20,6 +20,15 @@ namespace DateAndTimeProject
             Console.WriteLine($"Максимальное значение: {DateTime.MaxValue}");
 
             Console.WriteLine($"Текущее время: {DateTime.Now}");
+
+            // Создание объектов с конкретными заданными параметрами:
+
+            DateTime dateTime1 = new DateTime(2015, 12, 21);
+
+            DateTime dateTime2 = new DateTime(2017, 6, 15, 12, 45, 25);
+
+            Console.WriteLine(dateTime1);
+            Console.WriteLine(dateTime2);
         }
     }
 }
