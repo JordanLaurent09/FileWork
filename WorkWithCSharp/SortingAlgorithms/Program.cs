0c5464dba1149585180d242d8020242f80c94545
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-20 16:36:08 +0200

    Вспомогательная функция

--- WorkWithCSharp/SortingAlgorithms/Program.cs
+++ WorkWithCSharp/SortingAlgorithms/Program.cs
@@ -354,5 +354,44 @@ namespace SortingAlgorithms
             }
             return array;
         }
+
+        // Вспомогательная функция для сортировки слиянием
+        // Adding function for merge sorting algorithm
+
+        static void Merge(int[] array, int lowIndex, int middleIndex, int highIndex)
+        {
+            int left = lowIndex;
+            int right = middleIndex + 1;
+            int[] tempArray = new int[highIndex - lowIndex + 1];
+            int index = 0;
+            while((left <= middleIndex) && (right <= highIndex))
+            {
+                if(array[left] <= array[right])
+                {
+                    tempArray[index] = array[left];
+                    left++;
+                }
+                else
+                {
+                    tempArray[index] = array[right];
+                    right++;
+                }
+                index++;
+            }
+            for(int i = left; i <= middleIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+            for(int i = right; i <= highIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+            for(int i = 0; i < tempArray.Length; i++)
+            {
+                array[lowIndex + i] = tempArray[i];
+            }
+        }
     }
 }
