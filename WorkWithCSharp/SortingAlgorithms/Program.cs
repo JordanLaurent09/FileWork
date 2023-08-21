d5888ac7bc33da50e580afe529a5ee5220015adc
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-21 17:28:42 +0200

    Update Program.cs

--- WorkWithCSharp/SortingAlgorithms/Program.cs
+++ WorkWithCSharp/SortingAlgorithms/Program.cs
@@ -443,5 +443,20 @@ namespace SortingAlgorithms
             Swap(ref array[pivot], ref array[maxIndex]);
             return pivot;
         }
+
+        // Вспомогательная функция для быстрой сортировки
+        // Adding function for QUICK sorting algorithm
+
+        int[] QuickSort(int[]array, int minIndex, int maxIndex)
+        {
+            if(minIndex >= maxIndex)
+            {
+                return array;
+            }
+            int pivot = Partition(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivot - 1);
+            QuickSort(array, pivot + 1, maxIndex);
+            return array;
+        }
     }
 }
