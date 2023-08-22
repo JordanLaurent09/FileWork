8cadd1e8c34652628802292e5aa0823a5c26f126
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-22 19:35:47 +0200

    сортировки

--- WorkWithCSharp/SortingAlgorithms/Program.cs
+++ WorkWithCSharp/SortingAlgorithms/Program.cs
@@ -298,7 +298,39 @@ namespace SortingAlgorithms
             return QuickSort(array, 0, array.Length - 1);
         }
 
+        // Вспомогательная функция для быстрой сортировки (сортировки Хоара)
+        // Adding partition function for QUICK sorting algorithm
 
+        static int Partition(int[] array, int minIndex, int maxIndex)
+        {
+            int pivot = minIndex - 1;
+            for (int i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+
+        // Вспомогательная функция для быстрой сортировки
+        // Adding function for QUICK sorting algorithm
+
+        static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+            int pivot = Partition(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivot - 1);
+            QuickSort(array, pivot + 1, maxIndex);
+            return array;
+        }
         // Алгоритм "гномьей" сортировки
         // Gnome sorting algorithm
 
@@ -473,38 +505,38 @@ namespace SortingAlgorithms
         }
 
 
-        // Вспомогательная функция для быстрой сортировки (сортировки Хоара)
-        // Adding partition function for QUICK sorting algorithm
+        //// Вспомогательная функция для быстрой сортировки (сортировки Хоара)
+        //// Adding partition function for QUICK sorting algorithm
 
-        static int Partition(int[] array, int minIndex, int maxIndex)
-        {
-            int pivot = minIndex - 1;
-            for(int i = minIndex; i < maxIndex; i++)
-            {
-                if(array[i] < array[maxIndex])
-                {
-                    pivot++;
-                    Swap(ref array[pivot], ref array[i]);
-                }
-            }
-            pivot++;
-            Swap(ref array[pivot], ref array[maxIndex]);
-            return pivot;
-        }
+        //static int Partition(int[] array, int minIndex, int maxIndex)
+        //{
+        //    int pivot = minIndex - 1;
+        //    for(int i = minIndex; i < maxIndex; i++)
+        //    {
+        //        if(array[i] < array[maxIndex])
+        //        {
+        //            pivot++;
+        //            Swap(ref array[pivot], ref array[i]);
+        //        }
+        //    }
+        //    pivot++;
+        //    Swap(ref array[pivot], ref array[maxIndex]);
+        //    return pivot;
+        //}
 
-        // Вспомогательная функция для быстрой сортировки
-        // Adding function for QUICK sorting algorithm
+        //// Вспомогательная функция для быстрой сортировки
+        //// Adding function for QUICK sorting algorithm
 
-        static int[] QuickSort(int[]array, int minIndex, int maxIndex)
-        {
-            if(minIndex >= maxIndex)
-            {
-                return array;
-            }
-            int pivot = Partition(array, minIndex, maxIndex);
-            QuickSort(array, minIndex, pivot - 1);
-            QuickSort(array, pivot + 1, maxIndex);
-            return array;
-        }
+        //static int[] QuickSort(int[]array, int minIndex, int maxIndex)
+        //{
+        //    if(minIndex >= maxIndex)
+        //    {
+        //        return array;
+        //    }
+        //    int pivot = Partition(array, minIndex, maxIndex);
+        //    QuickSort(array, minIndex, pivot - 1);
+        //    QuickSort(array, pivot + 1, maxIndex);
+        //    return array;
+        //}
     }
 }
