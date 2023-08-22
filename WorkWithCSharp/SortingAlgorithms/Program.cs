5aeff887c41fe58e0847455252a7ae35e4224f91
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-22 19:36:54 +0200

    Update Program.cs

--- WorkWithCSharp/SortingAlgorithms/Program.cs
+++ WorkWithCSharp/SortingAlgorithms/Program.cs
@@ -298,8 +298,18 @@ namespace SortingAlgorithms
             return QuickSort(array, 0, array.Length - 1);
         }
 
-        // Вспомогательная функция для быстрой сортировки (сортировки Хоара)
-        // Adding partition function for QUICK sorting algorithm
+
+        static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+            int pivot = Partition(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivot - 1);
+            QuickSort(array, pivot + 1, maxIndex);
+            return array;
+        }
 
         static int Partition(int[] array, int minIndex, int maxIndex)
         {
@@ -317,20 +327,9 @@ namespace SortingAlgorithms
             return pivot;
         }
 
-        // Вспомогательная функция для быстрой сортировки
-        // Adding function for QUICK sorting algorithm
+        
 
-        static int[] QuickSort(int[] array, int minIndex, int maxIndex)
-        {
-            if (minIndex >= maxIndex)
-            {
-                return array;
-            }
-            int pivot = Partition(array, minIndex, maxIndex);
-            QuickSort(array, minIndex, pivot - 1);
-            QuickSort(array, pivot + 1, maxIndex);
-            return array;
-        }
+
         // Алгоритм "гномьей" сортировки
         // Gnome sorting algorithm
 
