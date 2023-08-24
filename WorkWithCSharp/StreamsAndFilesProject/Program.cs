f0d60028acaa44b315c8ef4066ca47aaf479ebe0
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-24 20:35:21 +0200

    Потоки и файлы

---
+++ WorkWithCSharp/StreamsAndFilesProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamsAndFilesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Синхронные потоки
+            // Чтение из файла
+            FileStream stream = null;
+            try
+            {
+                stream = new FileStream("File.txt", FileMode.Open);
+                byte[] buffer = new byte[1024];
+                UTF8Encoding text = new UTF8Encoding(true);
+
+                while(stream.Read(buffer, 0, buffer.Length) > 0)
+                {
+                    Console.WriteLine(text.GetString(buffer));
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                stream.Close();
+            }
+        }
+    }
+}
