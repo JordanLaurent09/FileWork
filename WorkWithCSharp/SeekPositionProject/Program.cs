8480a47a54415c7219a50a7db37156cf5d7fdef6
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-25 21:55:50 +0200

    Update Program.cs

--- WorkWithCSharp/SeekPositionProject/Program.cs
+++ WorkWithCSharp/SeekPositionProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SeekPositionProject
 {
@@ -12,9 +13,23 @@ namespace SeekPositionProject
         // производится чтение и запись. Имеет 2 параметра - offset (положение курсора)
         // и позиция в файле, описываемая тремя возможными значениями - 
         // SeekOrigin.Begin, SeekOrigin.End, SeekOrigin.Current
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
+            string text = "This is the TEXT";
+            using(FileStream fs = new FileStream("text.txt", FileMode.OpenOrCreate))
+            {
+                byte[] buffer = Encoding.Default.GetBytes(text);
+                fs.Write(buffer, 0, buffer.Length);
+            }
 
+            using(FileStream fs = new FileStream("text.txt",FileMode.OpenOrCreate))
+            {
+                fs.Seek(7, SeekOrigin.Begin);
+                byte[] buffer = new byte[10];
+                await fs.ReadAsync(buffer, 0, buffer.Length);
+                string textFromFile = Encoding.Default.GetString(buffer);
+                Console.WriteLine(textFromFile);
+            }
         }
     }
 }
