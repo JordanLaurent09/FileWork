05bb4ea74fe4c197fdfa12363b4bf6d3c972ee22
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-25 21:46:23 +0200

    Update Program.cs

--- WorkWithCSharp/SeekPositionProject/Program.cs
+++ WorkWithCSharp/SeekPositionProject/Program.cs
@@ -8,8 +8,13 @@ namespace SeekPositionProject
 {
     class Program
     {
+        //Seek() - позволяет управлять положением курсора потока, начиная с которого
+        // производится чтение и запись. Имеет 2 параметра - offset (положение курсора)
+        // и позиция в файле, описываемая тремя возможными значениями - 
+        // SeekOrigin.Begin, SeekOrigin.End, SeekOrigin.Current
         static void Main(string[] args)
         {
+
         }
     }
 }
