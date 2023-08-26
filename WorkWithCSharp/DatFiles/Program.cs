479c64d1b34d68221d2f2095374e4431e4cf7b09
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-26 21:39:33 +0200

    Update Program.cs

--- WorkWithCSharp/DatFiles/Program.cs
+++ WorkWithCSharp/DatFiles/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DatFiles
 {
@@ -10,6 +11,24 @@ namespace DatFiles
     {
         static void Main(string[] args)
         {
+            // Запись в .dat файл
+            using(BinaryWriter writer = new BinaryWriter(File.Open("binaryFile.dat", FileMode.OpenOrCreate)))
+            {
+                for(int i = 0; i < 5; i++)
+                {
+                    writer.Write(int.Parse(Console.ReadLine()));
+                }
+            }
+
+            // Чтение из .dat файла
+            using(BinaryReader reader = new BinaryReader(File.Open("binaryFile.dat", FileMode.Open)))
+            {
+                for(int i = 0; i < 5; i++)
+                {
+                    int digit = reader.ReadInt32();
+                    Console.WriteLine(digit);
+                }
+            }
         }
     }
 }
