fe791b3ae3ad271944c04f8dec5b3597b6c6db08
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-26 17:34:22 +0200

    stream work

---
+++ WorkWithCSharp/StreamsWork/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StreamsWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 Чтение данных из файла и их обработка по заданному условию
+            using(StreamReader sr = new StreamReader("text.txt"))
+            {
+                string line;
+                while((line = sr.ReadLine())!= null)
+                {
+                    if(line.StartsWith("T"))
+                    {
+                        Console.WriteLine(line);
+                    }
+                    if (line.Length > 30)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
