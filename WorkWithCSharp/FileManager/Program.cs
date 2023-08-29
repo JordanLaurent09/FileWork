3fb93ce30378f83f1a40031dcb100c49f8ca6f1a
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-29 18:58:48 +0200

    FileManager

---
+++ WorkWithCSharp/FileManager/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.CurrentDirectory;
+            while(true)
+            {
+                Console.Write(path + ">");
+                string command = Console.ReadLine();
+                string[] commands = command.Split(' ');
+                switch(commands[0])
+                {
+                    case "help":
+                        Console.WriteLine("touch-создание файла touch file");
+                        Console.WriteLine("copy-копирование файла copy source destination");
+                        Console.WriteLine("move-перемещение переименование файла move source destination");
+                        Console.WriteLine("delete-удаление файла delete file");
+                        break;
+
+                }
+            }
+        }
+    }
+}
