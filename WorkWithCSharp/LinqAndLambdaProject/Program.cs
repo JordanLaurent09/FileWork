cf3e8600aa8ad528c586f8aaf08eff58c6c23caf
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-08-30 18:22:38 +0200

    SomeLinq

---
+++ WorkWithCSharp/LinqAndLambdaProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAndLambdaProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Некоторые методы Linq
+
+            Console.WriteLine("Введите размер массива");
+            int length = int.Parse(Console.ReadLine());
+            int[] array = new int[length];
+            Random random = new Random();
+            for(int i = 0; i <  length; i++)
+            {
+                array[i] = random.Next(-10, 11);
+                Console.Write($"{array[i]} ");
+            }
+
+            Console.WriteLine();
+
+            //  Использование Linq
+
+            Console.WriteLine(array.Sum());
+            Console.WriteLine(array.Count());
+            Console.WriteLine(array.Average());
+            Console.WriteLine(array.Max());
+            Console.WriteLine(array.Min());
+        }
+    }
+}
