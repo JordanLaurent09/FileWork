a8419e5f86dd441063e898be6b0ae186d1acc579
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-09-20 16:57:45 +0200

    tree file system

--- WorkWithCSharp/FileSystemTreeProject/Program.cs
+++ WorkWithCSharp/FileSystemTreeProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FileSystemTreeProject
 {
@@ -10,6 +11,31 @@ namespace FileSystemTreeProject
     {
         static void Main(string[] args)
         {
+            string defaultPath = "C:\\";
+            PrintDirectory(new DirectoryInfo(defaultPath), "", true);
+        }
+
+        public static void PrintDirectory(DirectoryInfo directory, string indent, bool lastDirectory)
+        {
+            Console.Write(indent);
+            if(lastDirectory)
+            {
+                Console.Write("|___");
+                indent += " ";
+            }
+            else
+            {
+                Console.Write("|---");
+                indent += "| ";
+            }
+            Console.WriteLine(directory.Name);
+
+            DirectoryInfo[] subDirectories = directory.GetDirectories();
+
+            for (int i = 0; i < subDirectories.Length; i++)
+            {
+                PrintDirectory(subDirectories[i], indent, i == subDirectories.Length - 1);
+            }
         }
     }
 }
